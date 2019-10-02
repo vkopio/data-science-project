5fbd966c9a5931b41e00cd5df9aacf292996bd60
vkopio <ville.kopio@gmail.com>
2019-10-02 16:05:25 +0300

    Added functionality for loading geoJSON

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import { 
     Map, 
     GoogleApiWrapper, 
+    MapProps,
+    mapEventHandler,
     ProvidedProps, 
     GoogleApiOptions 
 } from 'google-maps-react'
 
-const MapContainer = (props: ProvidedProps & GoogleApiOptions) => {
+const MapContainer = (props: ProvidedProps & GoogleApiOptions): any => {
+    const loadGeoJSON: mapEventHandler = (
+        mapProps: MapProps | undefined,
+        map: google.maps.Map | undefined
+    ) => {
+        if (map !== undefined) {
+            map.data.loadGeoJson('https://storage.googleapis.com/mapsdevsite/json/google.json')
+        }
+    }
+
     return (
         <Map
           google={props.google}
           zoom={6}
           initialCenter={{lat: 60.171, lng: 24.937}}
+          onReady={loadGeoJSON}
         />
     );
 }
